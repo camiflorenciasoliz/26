08e4d034fc756a5d3d61e8aa2dc61b8a80cf98d6
Camila Honorio <camifloreciasoliz@gmail.com>
2026-05-29 08:31:15 -0300

    urls

--- Project/Api/urls.py
+++ Project/Api/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import*
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Api.urls'))
+    path('', Home,neme ="Inicio"),
 ]
